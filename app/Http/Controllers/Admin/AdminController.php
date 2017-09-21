81e3473235fbc3c4f84e8fa461da2bc21d3f9c6b
ydl <yangdeliang@juwang.cn>
2017-09-21 19:19:07 +0800

    x

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -19,7 +19,18 @@ class AdminController extends Controller
      */
     public function index()
     {
+        $name = '学院君';
+        $flag = \Mail::send('emails.test',['name'=>$name],function($message){
+            $to = '623601391@qq.com';
+            $message ->to($to)->subject('测试邮件');
+        });
+        if($flag){
+            echo '发送邮件成功，请查收！';
+        }else{
+            echo '发送邮件失败，请重试！';
+        }
 
+        exit;
         $user = \Auth::user();
         $role = Role::where(['name' => 'admin', ])->first();
         $user->attachRole($role);
